647e913f0c299ab56f65a2ea7db67fed0fcbfa7c
Ronaldo Junior <dev.ronaldojunior@gmail.com>
2024-02-23 11:46:02 -0300

    Adicionei as funções

---
+++ Calculadora/script.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay(){
+    document.getElementById('display').value = "";
+}
+
+function calculate(){
+    try{
+        var result = eval(document.getElementById('display').value);
+        if (isNaN(result) || !isFinite(result)) {
+            throw "Error";
+        }
+        document.getElementById('display').value = result;
+    } catch(error){
+        document.getElementById('display').value = 'Error';
+    }
+}
